482521edde60add73c04c06da3778c8629e1de41
Oussama AOULAD LAHCENE <62432095+AouladLahceneOussama@users.noreply.github.com>
2021-04-19 02:14:31 +0100

    Delete admin directory

--- admin/adminScripts/main.js
+++
@@ -1,9 +0,0 @@
-var menuBtn=document.getElementById("menu_btn");
-var nav=document.getElementById("header");
-var navList=document.querySelector("header nav ul");
-
-menuBtn.addEventListener("click",function(){
-    nav.classList.toggle("max_height");
-    nav.classList.toggle("min_height");
-    navList.classList.toggle("visible");
-});
